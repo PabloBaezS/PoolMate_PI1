48b990a1a6c23cd496c5e36972c41a4384d3554b
Pablo Baez Santamaria <pbaezs@eafit.edu.co>
2023-02-18 11:45:13 -0500

    BackEnd final

--- BackEnd.py
+++ BackEnd.py
@@ -1,13 +1,21 @@
-from flask_cors import CORS
-from flask import Flask, jsonify
-from processdata import processdata
+import googlemaps
 
+# Apik Key from GooGle Maps
+gmaps = googlemaps.Client(key='AIzaSyBaJ_KORpCWjJT8tP4N7L6VSRoHPHUTXFg')
 
-app = Flask(__name__)
-CORS(app)
-@app.route('/displaylocations')
-def displaylocations():
-    l = processdata()
-    return jsonify(l)
-if __name__ == '__main__':
-    BackEnd.run(host = <host_name>, debug = True, port = <port_no>)
+# Define the origin and destination addresses
+print('Enter your origin place!')
+origin = input('->')
+print('Enter your destination!')
+destination = input('->')
+
+print('Loading...')
+
+# Use the Google Maps API to get the distance and travel time between the two addresses
+result = gmaps.distance_matrix(origin, destination, mode='driving')
+
+# Print the distance and travel time
+distance = result['rows'][0]['elements'][0]['distance']['text']
+duration = result['rows'][0]['elements'][0]['duration']['text']
+print('The distance between', origin, 'and', destination, 'is', distance)
+print('The travel time between', origin, 'and', destination, 'is', duration)
--- processData.py
+++ processData.py
@@ -1,8 +1,1 @@
-# Importing the pandas package.
-import pandas as pd
-def processdata():
-    # Loading and reading the CSV file.
-    df = pd.read_csv('chipotle_stores.csv', delimiter = ',')
-    # Converting the CSV data to a list.
-    df = df.to_dict(orient = 'records')
-    return df
+#here we'll do the process data section using the Django DataBase and the BackEnd section to use improve the app!

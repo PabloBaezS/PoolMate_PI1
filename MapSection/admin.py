c1bb562f1ee6862813930a495084f3dc3e71b954
Juan Sebastian Camacho <100236682+juansecamacho@users.noreply.github.com>
2023-05-25 09:52:00 -0500

    Delete MapSection directory

--- MapSection/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from MapSection.models import Route
-
-admin.site.register(Route)
-
--- MapSection/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class RouteForm(forms.Form):
-    origin = forms.CharField(max_length=100)
-    destination = forms.CharField(max_length=100)
-
